96fcbf8ce6358a65de53a8350aeca03fbbedb3bb
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-04 10:12:59 -0500

    yes

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return (
+      <footer>
+        <p>2024 mora development. All rights reserved</p>
+      </footer>
+    )
+  }
+  
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header(){
+    return (
+      <header>
+        <nav>
+          <img src="logo512.png" alt="this is the logo of react"/>
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
